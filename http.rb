3a2dd29a439d5db1f536bdca89fa27b0e0a5a68a
dornby <hugo@drivy.com>
2018-10-09 22:24:30 +0200

    Iterating on code to make it more elegant

--- http.rb
+++ http.rb
@@ -21,19 +21,15 @@ elsif ARGV[0] == "create"
   Faraday.post('https://api.trello.com/1/cards', { :name => ARGV[1],
   :key => API_KEY_TRELLO,
   :token => TOKEN_TRELLO })
-elsif ARGV[0] == "delete" and Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
-:token => TOKEN_TRELLO }).status.between?(200, 299) == true
-  Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
-  :token => TOKEN_TRELLO })
-elsif ARGV[0] == "delete" and Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
-:token => TOKEN_TRELLO }).status.between?(200, 299) == false
-
-  puts "We couldn't find any card with the id "+ ARGV[1] + "\n Here is the list of existing cards:\n"
-
-  infos.each do |info|
-    puts info
+elsif ARGV[0] == "delete"
+  if Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
+    :token => TOKEN_TRELLO }).status.between?(200, 299) == true
+  else
+    puts "We couldn't find any card with the id "+ ARGV[1] + "\n Here is the list of existing cards:\n"
+    infos.each do |info|
+      puts info
+    end
   end
-
 else
   puts "error"
 end
