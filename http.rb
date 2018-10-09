9bf3cafdbf3af614d782309374cad2274fd4d9ed
dornby <hugo@drivy.com>
2018-10-09 20:37:04 +0200

    removing comment

--- http.rb
+++ http.rb
@@ -1,11 +1,7 @@
 require "faraday"
 
-# response = Faraday.get 'https://api.trello.com/1/boards/LFZoaH46/cards?fields=name,url&key=530770590241f292e896f80818f68e1d&token=513b5b9619d4777c692622e3728bb2dc9d3d185bc5fb587fd00506ec31a9dadd'
-# not using the token because public board
-
 response = Faraday.get 'https://api.trello.com/1/boards/LFZoaH46/cards?fields=name,url'
 
-# puts response.body
 json_cards = response.body
 
 require "json"
