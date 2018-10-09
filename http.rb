d87d202fff6fab9c3a7e3b7e5a79e70b174e913c
dornby <hugo@drivy.com>
2018-10-09 22:06:41 +0200

    commiting with gitignore

--- http.rb
+++ http.rb
@@ -1,3 +1,4 @@
+require_relative "secrets"
 require "faraday"
 
 response = Faraday.get 'https://api.trello.com/1/boards/LFZoaH46/cards?fields=name,url'
@@ -17,11 +18,16 @@ end
 if ARGV[0] == "list"
   puts infos
 elsif ARGV[0] == "create"
-  Faraday.post('https://api.trello.com/1/cards', { :name => 'Pas coder tant',
-  :desc => 'Encore une description',
-  :idList => '5bb39ee0029f335fb88aa2a9',
-  :key => '530770590241f292e896f80818f68e1d',
-  :token => '513b5b9619d4777c692622e3728bb2dc9d3d185bc5fb587fd00506ec31a9dadd' })
+  Faraday.post('https://api.trello.com/1/cards', { :name => ARGV[1],
+  :key => API_KEY_TRELLO,
+  :token => TOKEN_TRELLO })
+elsif ARGV[0] == "delete" and Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
+:token => TOKEN_TRELLO }).status.between?(200, 299) == true
+  Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
+  :token => TOKEN_TRELLO })
+elsif ARGV[0] == "delete" and Faraday.delete('https://api.trello.com/1/cards/' + ARGV[1], {:key => API_KEY_TRELLO,
+:token => TOKEN_TRELLO }).status.between?(200, 299) == false
+  puts "We couldn't find any card with the id "+ ARGV[1] + "\n Here is the list of existing cards:\n"
 else
   puts "error"
 end
